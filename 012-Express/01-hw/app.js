771c923e5e992171c1481bfafc188ad4f8c60129
TianYi <2861218857@qq.com>
2019-11-13 17:06:05 +0800

    19/11/13/闭包

--- 012-Express/01-hw/app.js
+++ 012-Express/01-hw/app.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require('express')   //引入express
+const app = express()     //express其实就是一个方法,创建app为express的实例
 
 //路由请求方法   get => 127.0.0.1:3000/
-app.get('/',(req,res) =>res.send('Hello world!'))
+//get后面的第一项是路由地址,第二项是根据请求地址做的响应
+app.get('/',(req,res) =>res.send('Hello world!'))  
 app.listen(3000,()=>console.log('Example app listening on port 3000!'))
--- 012-Express/02-static/app.js
+++ 012-Express/02-static/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 //处理静态资源
-// app.use(express.static('public'))  
+// app.use(express.static('public'))
+//所有的静态资源放在public文件夹中,写了处理静态资源的语句后,在访问根目录的时候会在public文件夹中寻找index.html文件
 app.use('/static', express.static('public'))  //设置了虚拟路径
 
 app.get('/',(req,res) =>res.send('Hello world!')) 
