757ec39fc736f9674541e7a992c5469137e2aa48
TianYi2861218857 <2861218857@qq.com>
2019-12-22 17:55:33 +0800

    19/12/22/user-center

--- 015-kmall/cs/home/src/pages/user-center/index.js
+++ 015-kmall/cs/home/src/pages/user-center/index.js
@@ -5,16 +5,15 @@ require('pages/common/side')
 require('pages/common/footer')
 require('./index.css')
 
-
 var page = {
-	init:function(){
-		// this.renderSide()
-	},
+	// init:function(){
+	// 	this.renderSide()
+	// },
 	// renderSide:function(){
 	// 	_side.render('user-center')
 	// }
 }
 
 $(function(){
-	page.init()
+	// page.init()
 })
